b1a3c7cf085080a8aa138cfdcc1a3b0e385bb49d
JoernT <joern.turner@gmail.com>
2021-06-25 16:00:25 +0200

    action running external javascript; demos;

--- src/actions/fx-action.js
+++ src/actions/fx-action.js
@@ -8,12 +8,43 @@ import { AbstractAction } from './abstract-action.js';
  * @demo demo/index.html
  */
 export class FxAction extends AbstractAction {
+
+  constructor() {
+    super();
+    this.attachShadow({ mode: 'open' });
+  }
+
+  connectedCallback() {
+    super.connectedCallback();
+    this.src = this.hasAttribute('src')?this.getAttribute('src'):null;
+    const style = `
+        :host{
+            display:none;
+        }
+    `;
+    this.shadowRoot.innerHTML = `
+        <style>
+            ${style}
+        </style>
+        <slot></slot>
+    `;
+  }
+
   perform() {
     const { children } = this;
-    Array.from(children).forEach(action => {
-      action.perform();
-    });
-    this.needsUpdate = true;
+
+    if(this.src){
+      this.innerHTML = ''; // reset
+      console.log('### fx-script.perform ');
+      const script = document.createElement('script');
+      script.src = this.src;
+      this.appendChild(script);
+    } else {
+      Array.from(children).forEach(action => {
+        action.perform();
+      });
+      this.needsUpdate = true;
+    }
   }
 }
 
