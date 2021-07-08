cee7bcc46ff5e16a352c8b328b50600011f30b06
JoernT <joern.turner@gmail.com>
2021-07-08 19:08:07 +0200

    rewritten fx-instance to use fetch API instead of iron-ajax; now also handling remote json loading; added test; dropped iron-ajax dependency;

--- src/fx-instance.js
+++ src/fx-instance.js
@@ -1,35 +1,36 @@
-import '@polymer/iron-ajax/iron-ajax.js';
+import {evaluateXPathToFirstNode} from './xpath-evaluation.js';
 
-import { evaluateXPathToFirstNode } from './xpath-evaluation.js';
-
+/**
+ * Container for data instances.
+ *
+ * Offers several ways of loading data from either inline content or via 'src' attribute which will use the fetch
+ * API to resolve data.
+ */
 export class FxInstance extends HTMLElement {
-  constructor() {
-    super();
-
-    this.model = this.parentNode;
-
-    this.attachShadow({ mode: 'open' });
-  }
-
-  connectedCallback() {
-    // console.log('connectedCallback ', this);
-    if (this.hasAttribute('src')) {
-      this.src = this.getAttribute('src');
-    }
-    // this.src = '';
-
-    if (this.hasAttribute('id')) {
-      this.id = this.getAttribute('id');
-    } else {
-      this.id = 'default';
+    constructor() {
+        super();
+        this.model = this.parentNode;
+        this.attachShadow({mode: 'open'});
     }
 
-    if (this.hasAttribute('type')) {
-      this.type = this.getAttribute('type');
-    } else {
-      this.type = 'xml';
-    }
-    const style = `
+    connectedCallback() {
+        // console.log('connectedCallback ', this);
+        if (this.hasAttribute('src')) {
+            this.src = this.getAttribute('src');
+        }
+
+        if (this.hasAttribute('id')) {
+            this.id = this.getAttribute('id');
+        } else {
+            this.id = 'default';
+        }
+
+        if (this.hasAttribute('type')) {
+            this.type = this.getAttribute('type');
+        } else {
+            this.type = 'xml';
+        }
+        const style = `
             :host {
                 display: none;
             }
@@ -41,149 +42,173 @@ export class FxInstance extends HTMLElement {
             }
         `;
 
-    const html = `
-           <iron-ajax
-                id="loader"
-                url="${this.src}"
-                method="GET"
-                handle-as="text"
-                with-credentials></iron-ajax>
+        const html = `
         `;
-    this.shadowRoot.innerHTML = `
+        this.shadowRoot.innerHTML = `
             <style>
                 ${style}
             </style>
             ${html}
         `;
-  }
-
-  async init() {
-    // console.log('fx-instance init');
-    // if(this.src) return;
-    if (this.type === 'xml') {
-      await this._initXMLInstance();
-    } else {
-      this._initJSONInstance();
     }
-    this.dispatchEvent(
-      new CustomEvent('instance-loaded', {
-        composed: true,
-        bubbles: true,
-        detail: { instance: this },
-      }),
-    );
 
-    return this;
-    // this.shadowRoot.getElementById('data').appendChild(this.instanceData.cloneNode(true));
-  }
-
-  evalXPath(xpath) {
-    const formElement = this.parentElement.parentElement;
-    const result = evaluateXPathToFirstNode(xpath, this.getDefaultContext(), formElement);
-    return result;
-  }
-
-  getInstanceData() {
-    return this.instanceData;
-  }
-
-  getDefaultContext() {
-    // console.log('getDefaultContext ', this.instanceData.firstElementChild);
-    if (this.type === 'xml') {
-      return this.instanceData.firstElementChild;
+    /**
+     * Is called by fx-model during initialization phase (model-construct)
+     * @returns {Promise<void>}
+     */
+    async init() {
+        // console.log('fx-instance init');
+        await this._initInstance().then(() =>{
+            this.dispatchEvent(
+                new CustomEvent('instance-loaded', {
+                    composed: true,
+                    bubbles: true,
+                    detail: {instance: this},
+                }),
+            );
+            return this;
+        });
     }
-    return this.instanceData;
-  }
 
-  _initXMLInstance() {
-    const loadedPromise = new Promise((resolve, reject) => {
-      // setTimeout(() => resolve("done"), 2000);
+    evalXPath(xpath) {
+        const formElement = this.parentElement.parentElement;
+        const result = evaluateXPathToFirstNode(xpath, this.getDefaultContext(), formElement);
+        return result;
+    }
 
-      if (this.src === '#querystring') {
-        // eslint-disable-next-line no-restricted-globals
-        const query = new URLSearchParams(location.search);
-        // console.log('query', query);
+    /**
+     * returns the current instance data
+     *
+     * @returns {Document | T | any}
+     */
+    getInstanceData() {
+        return this.instanceData;
+    }
 
-        const doc = new DOMParser().parseFromString('<data></data>', 'application/xml');
-        const root = doc.firstElementChild;
-        for (const p of query) {
-          const newNode = doc.createElement(p[0]);
-          newNode.appendChild(doc.createTextNode(p[1]));
-          root.appendChild(newNode);
+    /**
+     * return the default context (root node of respective instance) for XPath evalution.
+     *
+     * @returns {Document|T|any|Element}
+     */
+    getDefaultContext() {
+        // console.log('getDefaultContext ', this.instanceData.firstElementChild);
+        if (this.type === 'xml') {
+            return this.instanceData.firstElementChild;
         }
+        return this.instanceData;
+    }
 
-        this.instanceData = doc;
-        this.instanceData.firstElementChild.setAttribute('id', this.id);
+    /**
+     * does the actual loading of data. Handles inline data, data loaded via fetch() or data constructed from
+     * querystring.
+     *
+     * @returns {Promise<void>}
+     * @private
+     */
+    async _initInstance() {
+        if (this.src === '#querystring') {
+            /*
+            * generate XML data from URL querystring
+            * todo: there's no variant to generate JSON yet
+            */
+            // eslint-disable-next-line no-restricted-globals
+            const query = new URLSearchParams(location.search);
+            const doc = new DOMParser().parseFromString('<data></data>', 'application/xml');
+            const root = doc.firstElementChild;
+            for (const p of query) {
+                const newNode = doc.createElement(p[0]);
+                newNode.appendChild(doc.createTextNode(p[1]));
+                root.appendChild(newNode);
+            }
+            this.instanceData = doc;
+            this.instanceData.firstElementChild.setAttribute('id', this.id);
+            // resolve('done');
+        } else if (this.src) {
+            await this._loadData();
+        } else if (this.childNodes.length !== 0) {
+            this._useInlineData();
+        }
+    }
 
-        // const data = new XMLSerializer().serializeToString(this.instanceData);
-        // console.log('instance from querystring ' , data);
+    async _loadData() {
+        const url = `${this.src}`;
+        const contentType = this._getContentType();
+        await fetch(url, {
+            method: 'GET',
+            mode: 'cors',
+            credentials: 'same-origin',
+            headers: {
+                'Content-Type': contentType
+            }
+        }).then((response) => {
+            const responseContentType = response.headers.get("content-type").toLowerCase();
+            console.log("********** responseContentType *********", responseContentType);
+            if (responseContentType.startsWith("text/plain")) {
+                // console.log("********** inside  res plain *********");
+                return response.text();
+            } else if (responseContentType.startsWith("application/json")) {
+                // console.log("********** inside res json *********");
+                return response.json();
+            } else if (responseContentType.startsWith("application/xml")) {
+                return response.text().
+                then((result) => {
+                    console.log("xml ********",result);
+                    return new DOMParser().parseFromString(
+                        result,
+                        'application/xml',
+                    );
+                });
+            }
+        }).then(data => {
+            this.instanceData = data;
+            console.log('instanceData loaded: ', this.instanceData);
+        }).catch((error) => {
+            throw new Error(`failed loading data ${error}`);
+        });
+    }
 
-        resolve('done');
-      } else if (this.src) {
+    _getContentType() {
+        if (this.type === 'xml') {
+            return 'application/xml'
+        } else if (this.type === 'json') {
+            return 'application/json';
+        } else {
+            console.warn('content-type unknown ', this.type);
+            return null;
+        }
+    }
+
+    _useInlineData() {
+        if (this.type === 'xml') {
+            // console.log('innerHTML ', this.innerHTML);
+            const instanceData = new DOMParser().parseFromString(this.innerHTML, 'application/xml');
+
+            console.log('fx-instance init id:', this.id);
+            this.instanceData = instanceData;
+            // console.log('instanceData ', this.instanceData);
+            // console.log('instanceData ', this.instanceData.firstElementChild);
+
+            console.log('fx-instance data: ', this.instanceData);
+            this.instanceData.firstElementChild.setAttribute('id', this.id);
+            // todo: move innerHTML out to shadowDOM (for later reset)
+        } else if (this.type === 'json') {
+            this.instanceData = JSON.parse(this.textContent);
+        } else {
+            console.warn('unknow type for data ', this.type);
+        }
+    }
+
+    _handleResponse() {
+        console.log('_handleResponse ');
+        const ajax = this.shadowRoot.getElementById('loader');
+        const instanceData = new DOMParser().parseFromString(ajax.lastResponse, 'application/xml');
+        this.instanceData = instanceData;
+        console.log('data: ', this.instanceData);
+    }
+
+    _handleError() {
         const loader = this.shadowRoot.getElementById('loader');
-        loader.addEventListener('response', () => {
-          const instanceData = new DOMParser().parseFromString(
-            loader.lastResponse,
-            'application/xml',
-          );
-          this.instanceData = instanceData;
-          console.log('fx-instance data: ', this.instanceData);
-          this.dispatchEvent(new CustomEvent('instance-loaded', {}));
-          resolve(instanceData);
-        });
-        loader.addEventListener('error', () => {
-          console.log('error while loading data from src: ', loader.lastError);
-          reject();
-        });
-        loader.generateRequest();
-      } else if (this.childNodes.length !== 0) {
-        // setTimeout(() => resolve("done"), 2000);
-        // var foo = this;
-        // setTimeout(function(){
-        //     foo._useInlineData();
-        //     resolve("done");
-        // },10000);
-
-        this._useInlineData();
-        resolve('done');
-      }
-    });
-    return loadedPromise;
-  }
-
-  _initJSONInstance() {
-    this.instanceData = JSON.parse(this.textContent);
-  }
-
-  _useInlineData() {
-    // console.log('innerHTML ', this.innerHTML);
-    const instanceData = new DOMParser().parseFromString(this.innerHTML, 'application/xml');
-
-    // console.log('created instanceData ', new XMLSerializer().serializeToString(instanceData));
-    // console.log('namespace ', instanceData.firstElementChild.namespaceURI);
-
-    console.log('fx-instance init id:', this.id);
-    this.instanceData = instanceData;
-    // console.log('instanceData ', this.instanceData);
-    // console.log('instanceData ', this.instanceData.firstElementChild);
-    // this.shadowRoot.appendChild(this.instanceData.firstElementChild);
-
-    console.log('fx-instance data: ', this.instanceData);
-    this.instanceData.firstElementChild.setAttribute('id', this.id);
-    // console.log('fx-instance data ', this.instanceData);
-  }
-
-  _handleResponse() {
-    console.log('_handleResponse ');
-    const ajax = this.shadowRoot.getElementById('loader');
-    const instanceData = new DOMParser().parseFromString(ajax.lastResponse, 'application/xml');
-    this.instanceData = instanceData;
-    console.log('data: ', this.instanceData);
-  }
-
-  _handleError() {
-    const loader = this.shadowRoot.getElementById('loader');
-    console.log('_handleResponse ', loader.lastError);
-  }
+        console.log('_handleResponse ', loader.lastError);
+    }
 }
-customElements.define('fx-instance', FxInstance);
+customElements.define('fx-instance',FxInstance);
