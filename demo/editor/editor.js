2610324718e0f3c4d8062225eccb6af610361a45
Joern Turner <joern.turner@gmail.com>
2021-09-03 18:48:20 +0200

    started editor exploration

---
+++ demo/editor/editor.js
@@ -0,0 +1,41 @@
+console.log('editor.js');
+
+document.addEventListener('dragover', function(e) {
+    e.preventDefault();
+    e.dataTransfer.dropEffect = "copy";
+});
+
+document.addEventListener('drop', function (e) {
+    console.log('drop',e);
+    // e.preventDefault();
+    // Get the id of the target and add the moved element to the target's DOM
+    const elementName = e.dataTransfer.getData("text/plain");
+    console.log('drop element',elementName);
+
+    const currentInsertPoint = document.querySelector('[current]');
+    const newElem = document.createElement(elementName);
+    newElem.setAttribute('data-name',elementName);
+    newElem.textContent = elementName;
+    currentInsertPoint.appendChild(newElem);
+
+    // const link = e.dataTransfer.getData("application/x-bookmark");
+    // const iconsrc = e.dataTransfer.getData("application/x-icon");
+
+});
+
+(function () {
+    console.log('inited page.....')
+    const draggables = document.querySelectorAll('.element-name')
+    draggables.forEach(element => {
+        element.addEventListener("dragstart", ev =>{
+            console.log('_handleDragStart',ev);
+            console.log('_handleDragStart target',ev.target);
+            console.log('_handleDragStart target',ev.target.innerText);
+            ev.dataTransfer.setData("text/plain", ev.target.innerText);
+
+        });
+    });
+
+
+
+})();
