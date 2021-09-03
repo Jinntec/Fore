85a16a8b2ab6322e0fc8bdc2a623a9e2e3c49c74
Joern Turner <joern.turner@gmail.com>
2021-09-03 18:52:28 +0200

    fixed typo

--- src/actions/abstract-action.js
+++ src/actions/abstract-action.js
@@ -133,4 +133,4 @@ export class AbstractAction extends foreElementMixin(HTMLElement) {
   }
 }
 
-window.customElements.define('abstracdt-action', AbstractAction);
+window.customElements.define('abstract-action', AbstractAction);
