5fedb0280041d9ddd29df260772253931abe5969
Joern Turner <joern.turner@gmail.com>
2021-09-20 17:50:29 +0200

    updated and fixed demo branch

--- demo/demo.js
+++ demo/demo.js
@@ -1,6 +1,1 @@
-import './node_modules/@polymer/iron-demo-helpers/demo-snippet.js';
-import './node_modules/@polymer/iron-demo-helpers/demo-pages-shared-styles.js';
-import './node_modules/@polymer/paper-input/paper-input.js';
-import './node_modules/@polymer/paper-checkbox/paper-checkbox.js';
-import './node_modules/@polymer/paper-button/paper-button.js';
-import '../index.js';
+import '../dist/fore-demo.js';
