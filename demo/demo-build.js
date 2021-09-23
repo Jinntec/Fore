e51087fbf8a1033a6981889c31f944839833d3aa
Joern Turner <joern.turner@gmail.com>
2021-09-23 14:24:26 +0200

    merging deps from 'demo' dir into root package.json

--- demo/demo-build.js
+++ demo/demo-build.js
@@ -1,8 +1,7 @@
-import './node_modules/@polymer/polymer';
-import './node_modules/@polymer/paper-input/paper-input.js';
-import './node_modules/@polymer/paper-checkbox/paper-checkbox.js';
-import './node_modules/@polymer/paper-button/paper-button.js';
-import './node_modules/@polymer/iron-demo-helpers/demo-snippet.js';
-import './node_modules/@polymer/iron-demo-helpers/demo-pages-shared-styles.js';
+import '../node_modules/@polymer/paper-input/paper-input.js';
+import '../node_modules/@polymer/paper-checkbox/paper-checkbox.js';
+import '../node_modules/@polymer/paper-button/paper-button.js';
+import '../node_modules/@polymer/iron-demo-helpers/demo-snippet.js';
+import '../node_modules/@polymer/iron-demo-helpers/demo-pages-shared-styles.js';
 
 import '../index.js';
