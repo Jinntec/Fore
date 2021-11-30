8bec74f47be7ee1bb0bacda4f457527872bb6c18
Magdalena Turska <tuurma@gmail.com>
2021-11-30 13:19:10 +0100

    attempt to get ling to have autocompleteßlike behaviour

--- demo/demo.js
+++ demo/demo.js
@@ -1,6 +1,6 @@
-import './node_modules/@polymer/iron-demo-helpers/demo-snippet.js';
-import './node_modules/@polymer/iron-demo-helpers/demo-pages-shared-styles.js';
-import './node_modules/@polymer/paper-input/paper-input.js';
-import './node_modules/@polymer/paper-checkbox/paper-checkbox.js';
-import './node_modules/@polymer/paper-button/paper-button.js';
+import '@polymer/iron-demo-helpers/demo-snippet.js';
+import '@polymer/iron-demo-helpers/demo-pages-shared-styles.js';
+import '@polymer/paper-input/paper-input.js';
+import '@polymer/paper-checkbox/paper-checkbox.js';
+import '@polymer/paper-button/paper-button.js';
 import '../index.js';
