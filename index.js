3bcacab71e69dece111157a7113ffb1904f2fa64
Joern Turner <joern.turner@gmail.com>
2021-12-22 17:01:12 +0100

    fixed build again - use fore-debug.js (with console) or fore-all.js (without)

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+import '@polymer/paper-input/paper-input.js';
+import '@polymer/paper-button/paper-button.js';
+import '@polymer/paper-checkbox/paper-checkbox.js';
+import '@polymer/paper-dialog/paper-dialog.js';
+
+
 // core + models classes
 import './src/fx-bind.js';
 import './src/fx-fore.js';
