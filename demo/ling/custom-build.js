ca6749aebf7c94eded7e77172554d7f966e65730
Joern Turner <joern.turner@gmail.com>
2022-02-03 15:59:39 +0100

    demos

--- demo/ling/custom-build.js
+++
@@ -1,6 +0,0 @@
-import '@polymer/paper-input/paper-input.js';
-import '@polymer/paper-button/paper-button.js';
-import '@polymer/paper-checkbox/paper-checkbox.js';
-import '@polymer/paper-dialog/paper-dialog.js';
-
-import '../../index.js';
