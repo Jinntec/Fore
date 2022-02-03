58ce6d97c56a90e0b0f1c4e32edde054b1d3a284
Joern Turner <joern.turner@gmail.com>
2022-02-03 17:48:26 +0100

    edited demos.html

--- demo/demo.js
+++ demo/demo.js
@@ -1,1 +1,6 @@
-import '../dist/fore-demo.js';
+import '@polymer/iron-demo-helpers/demo-snippet.js';
+import '@polymer/iron-demo-helpers/demo-pages-shared-styles.js';
+import '@polymer/paper-input/paper-input.js';
+import '@polymer/paper-checkbox/paper-checkbox.js';
+import '@polymer/paper-button/paper-button.js';
+import '../index.js';
