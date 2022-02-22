b64c1e13d1c06863d6daf86ab0529b010b387801
Joern Turner <joern.turner@gmail.com>
2022-02-22 11:10:43 +0100

    refreshing after falsy validation during submission to allow alerts to show

--- src/fx-submission.js
+++ src/fx-submission.js
@@ -81,6 +81,8 @@ export class FxSubmission extends foreElementMixin(HTMLElement) {
       const valid = model.revalidate();
       if (!valid) {
         console.log('validation failed. Bubmission stopped');
+        // ### allow alerts to pop up
+        this.getModel().parentNode.refresh();
         return;
       }
     }
