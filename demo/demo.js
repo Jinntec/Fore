a72570628917d28cab98acd712a7bb18c3ff11d9
Joern Turner <joern.turner@gmail.com>
2022-06-01 15:30:49 +0200

    adding dist using dist in demos

--- demo/demo.js
+++ demo/demo.js
@@ -4,4 +4,4 @@ import 'https://cdn.jsdelivr.net/npm/@polymer/iron-demo-helpers@3.1.0/demo-pages
 import 'https://cdn.jsdelivr.net/npm/@polymer/paper-input@3.2.1/paper-input.js';
 import 'https://cdn.jsdelivr.net/npm/@polymer/paper-checkbox@3.1.0/paper-checkbox.js';
 import 'https://cdn.jsdelivr.net/npm/@polymer/paper-button/paper-button.js';
-import '../index.js';
+import '../dist/fore.js';
