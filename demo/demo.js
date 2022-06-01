51e611065868d3f89c171b95f05048baec7d7475
Joern Turner <joern.turner@gmail.com>
2022-06-01 15:22:24 +0200

    changed import to use CDN

--- demo/demo.js
+++ demo/demo.js
@@ -1,6 +1,6 @@
 // import '@polymer/iron-demo-helpers/demo-snippet.js';
 import './js/demo-snippet.js';
-import '@polymer/iron-demo-helpers/demo-pages-shared-styles.js';
+import 'https://cdn.jsdelivr.net/npm/@polymer/iron-demo-helpers@3.1.0/demo-pages-shared-styles.js';
 import '@polymer/paper-input/paper-input.js';
 import '@polymer/paper-checkbox/paper-checkbox.js';
 import '@polymer/paper-button/paper-button.js';
