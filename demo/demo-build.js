69da776d8e65dded7795041d7181e36708b12a71
Joern Turner <joern.turner@gmail.com>
2022-06-10 11:40:26 +0200

    removed unnecessary import of webcomponen-loader.js

--- demo/demo-build.js
+++ demo/demo-build.js
@@ -1,7 +1,1 @@
-import '../node_modules/@polymer/paper-input/paper-input.js';
-import '../node_modules/@polymer/paper-checkbox/paper-checkbox.js';
-import '../node_modules/@polymer/paper-button/paper-button.js';
-import '../node_modules/@polymer/iron-demo-helpers/demo-snippet.js';
-import '../node_modules/@polymer/iron-demo-helpers/demo-pages-shared-styles.js';
-
-import '../index.js';
+// n.a. for dev branch
