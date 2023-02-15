4e9ee4708ee1f13e2739004aafd9b3f27e3bdc9c
duncdrum <d.paterson@me.com>
2023-02-15 21:50:29 +0100

    🧪 test(while): go through demos

--- cypress/e2e/while.cy.ts
+++ cypress/e2e/while.cy.ts
@@ -1,7 +1,38 @@
 describe('while demo spec', () => {
-
-  // @see https://www.thisdot.co/blog/testing-web-components-with-cypress-and-typescript
-  it('passes', () => {
+   beforeEach (() => {
     cy.visit('while.html')
+   })
+  // @see https://www.thisdot.co/blog/testing-web-components-with-cypress-and-typescript
+  it('show the expecter header', () => {
+    cy.get('h1 > code')
+      .should('contain', 'while')
+  })
+
+  it('display notification at 10', () => {
+    cy.get('[data-cy="counter"]')
+      .click()
+    cy.get('.toastify')
+      .should('be.visible')
+  })
+
+  it('notify user 10 times', () => {
+    cy.get('[data-cy="start"]')
+      .click()
+    cy.get('.toastify')
+      .should('have.length', '10')
+  })
+
+  it.skip('start and abort', () => {
+    cy.get('[data-cy="hey-go"]')
+      .click()
+    cy.get('.toastify')
+      .should('have.length', '10') 
+  })
+
+  it.skip('continue and abort on adding and removing', () => {
+    cy.get('[data-cy="hey-go"]')
+      .click()
+    cy.get('.toastify')
+      .should('have.length', '10') 
   })
 })
