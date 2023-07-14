62ecadd8dcd7255351a7ed74f3f45ecd8dba3a98
Joern Turner <joern@existsolutions.com>
2023-07-14 17:14:14 +0200

    * enhanced submission-relevance.html * enhanced relevance selection to consider empty attribures as non-relevant by default. This can be changed with nonrelevant="empty | keep" * added tests

--- src/relevance.js
+++ src/relevance.js
@@ -38,6 +38,19 @@ export class Relevance {
         const { attributes } = n;
         if (attributes) {
           Array.from(attributes).forEach(attr => {
+            if (element.nonrelevant === 'empty' && !Relevance._isRelevant(element, attr)) {
+              clone.setAttribute(attr.nodeName, '');
+            }else
+            if (Relevance._isRelevant(element, attr)) {
+              clone.setAttribute(attr.nodeName, attr.value);
+            } else {
+              // if (element.nonrelevant === 'empty') {
+              //   clone.setAttribute(attr.nodeName, '');
+              // } else{
+                clone.removeAttribute(attr.nodeName);
+              // }
+            }
+/*
             if (Relevance._isRelevant(element, attr)) {
               clone.setAttribute(attr.nodeName, attr.value);
             } else if (element.nonrelevant === 'empty') {
@@ -45,6 +58,7 @@ export class Relevance {
             } else {
               clone.removeAttribute(attr.nodeName);
             }
+*/
           });
         }
         return Relevance._filterRelevant(element, n, clone);
@@ -56,9 +70,21 @@ export class Relevance {
 
   static _isRelevant(element, node) {
     const mi = element.getModel().getModelItem(node);
-    if (!mi || mi.relevant) {
+
+    // ### remove empty attributes as these usually are not expected in most XML languages.
+    if(node.nodeType === Node.ATTRIBUTE_NODE
+        && node.nodeValue === ''){
+      return false;
+    }
+    // ### no modelItem means no constraints
+    if(!mi){
       return true;
     }
+    // ### modelItem 'relevant' is defined and 'true'
+    if ( mi.relevant) {
+      return true;
+    }
+
     return false;
   }
 }
