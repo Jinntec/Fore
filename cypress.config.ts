056bfa6700c1f88eb42be5986415915a02778a20
Martin Middel <martin.middel@fontoxml.com>
2023-10-03 16:25:37 +0200

    Fix user agent string

--- cypress.config.ts
+++ cypress.config.ts
@@ -8,4 +8,6 @@ export default defineConfig({
 			// implement node event listeners here
 		},
 	},
+	// The Cypress user agent breaks es-dev-server
+	userAgent: 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36'
 });
