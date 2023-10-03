3cb638304fdf6ea1cfbb9b3e16ea89a591b60f7f
Martin Middel <martin.middel@fontoxml.com>
2023-10-03 16:45:00 +0200

    Add test for url overwrite demo

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from "cypress";
 export default defineConfig({
 	e2e: {
 		baseUrl: 'http://localhost:8090/demo/',
+		experimentalStudio: true,
 
 		setupNodeEvents(on, config) {
 			// implement node event listeners here
