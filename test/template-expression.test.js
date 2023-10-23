7b163f7052effc95167ba4f10e35315cd3bf27ee
Joern Turner <joern@existsolutions.com>
2023-10-23 19:06:01 +0200

    added support for 'ignore-expressions' attribute taking a querySelector to identify some nodes to NOT consider for template evaluation.

--- test/template-expression.test.js
+++ test/template-expression.test.js
@@ -222,4 +222,47 @@ lines
 
 		expect(theDiv.textContent).to.equal('Braces! I contain {braces}');
 	});
+
+	it('does ignore elements matched by ignore-expression attribute', async () => {
+		const el = await fixtureSync(html`
+          <fx-fore ignore-expressions=".myElement">
+            <fx-model>
+              <fx-instance>
+                <data>
+                  <greeting>Hello</greeting>
+                  <ignored>if you read this it does not work</ignored>
+                </data>
+              </fx-instance>
+            </fx-model>
+
+            <div id="one">{greeting}</div>
+            <div class="myElement">
+              {ignored}{{whatever}}
+            </div>
+
+
+            <div class="myElement">
+              {}{}{{{}}}
+            </div>
+
+            <div class="myElement">
+              <span>{}{}{{{}}}</span>
+            </div>
+
+          </fx-fore>    `);
+
+		await oneEvent(el, 'refresh-done');
+
+		const theDiv = el.querySelector('#one');
+		expect(theDiv.textContent).to.equal('Hello');
+
+        const myElements = Array.from(document.querySelectorAll('.myElement'));
+        expect(myElements.length).to.equal(3);
+        console.log('####',myElements[0].textContent.trim())
+
+        expect(myElements[0].textContent.trim()).to.equal('{ignored}{{whatever}}')
+        expect(myElements[1].textContent.trim()).to.equal('{}{}{{{}}}')
+        expect(myElements[2].textContent.trim()).to.equal('{}{}{{{}}}')
+
+	});
 });
