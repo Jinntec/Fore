7a0531e45b6ba22039bfbe3d1c26c67519933ccf
Joern Turner <joern@existsolutions.com>
2023-11-02 13:45:45 +0100

    fix demo build to include version tagging

--- rollup.config.js
+++ rollup.config.js
@@ -3,42 +3,12 @@ import babel from 'rollup-plugin-babel';
 import { terser } from 'rollup-plugin-terser';
 import minifyHTML from 'rollup-plugin-minify-html-literals';
 import strip from '@rollup/plugin-strip';
+import versionInjector from 'rollup-plugin-version-injector';
 
 // eslint-disable-next-line no-unused-vars
 const { dependencies } = require('./package.json');
 
 export default [
-/*
-  {
-    input: './index.js',
-    output: [
-      {
-        file: 'dist/fore.js',
-        format: 'es',
-        sourcemap: true,
-      },
-    ],
-    external: moduleName =>
-      // All absolute imports should be regarded as external. Examples are 'fontoxpath',
-      // 'lit-element' or '@polymer/!*'
-      !/^(\.\/|\.\.\/)/.test(moduleName),
-    plugins: [
-      resolve(),
-      babel({
-        babelrc: false,
-        exclude: 'node_modules/!**',
-        plugins: [
-          // Tell babel to accept the `static READONLY_DEFAULT = false;` properties found in some places.
-          // TODO: reconsider whether that is a good idea.
-          // eslint-disable-next-line global-require
-          [require('@babel/plugin-proposal-class-properties'), { loose: true }],
-        ],
-      }),
-      minifyHTML(),
-      terser(),
-    ],
-  },
-*/
   {
     input: './demo/demo-build.js',
     output: [
@@ -49,6 +19,7 @@ export default [
       },
     ],
     plugins: [
+      versionInjector(),
       resolve(),
       babel({
         babelrc: false,
