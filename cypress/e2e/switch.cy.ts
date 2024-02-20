940c11a0b25faa694667aafd3334d47ff4fec46a
Martin Middel <martin.middel@fontoxml.com>
2024-02-20 15:01:55 +0100

    Add test to prevent regression with template expressions in switches

---
+++ cypress/e2e/switch.cy.ts
@@ -0,0 +1,26 @@
+describe('switch demo spec', () => {
+   beforeEach(() => {
+       cy.visit('switch.html');
+   });
+  it('should be able to switch between items', () => {
+	  cy.get('fx-switch.second').scrollIntoView();
+	  cy.get('fx-switch.second [name="page1"]').should('not.be.visible');
+	  cy.get('fx-switch.second [name="page2"]').should('be.visible');
+	  cy.get('fx-switch.second [name="page3"]').should('not.be.visible');
+	  cy.get('fx-switch.second [name="page2"]').should('contain.text', 'Page 2 (data is page2)');
+
+      cy.get('select.widget').select('page3')
+
+	  cy.get('fx-switch.second [name="page1"]').should('not.be.visible');
+	  cy.get('fx-switch.second [name="page2"]').should('not.be.visible');
+	  cy.get('fx-switch.second [name="page3"]').should('be.visible');
+	  cy.get('fx-switch.second [name="page3"]').should('contain.text', 'Page 3 (data is page3)');
+
+	  cy.get('select.widget').select('page1')
+
+	  cy.get('fx-switch.second [name="page1"]').should('be.visible');
+	  cy.get('fx-switch.second [name="page2"]').should('not.be.visible');
+	  cy.get('fx-switch.second [name="page3"]').should('not.be.visible');
+	  cy.get('fx-switch.second [name="page1"]').should('contain.text', 'Page 1 (data is page1)');
+  });
+});
