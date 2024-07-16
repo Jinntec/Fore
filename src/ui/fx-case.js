b3708dc32202054e26a388ca6c384bc38565d505
Joern Turner <joern@existsolutions.com>
2024-07-16 14:39:15 +0200

    some demos + fx-case extension for loading from src

--- src/ui/fx-case.js
+++ src/ui/fx-case.js
@@ -1,6 +1,7 @@
 // import { foreElementMixin } from '../ForeElementMixin';
 
 import {FxContainer} from "./fx-container.js";
+import {Fore} from "../fore.js";
 
 /**
  * `fx-case`
@@ -10,42 +11,87 @@ import {FxContainer} from "./fx-container.js";
  * @customElement
  */
 class FxCase extends FxContainer {
-/*
-  constructor() {
-    super();
-  }
-*/
+    static get properties() {
+        return {
+            ...super.properties,
+            label:{
+                type: String
+            },
+            name:{
+                type: String
+            },
+            selected:{
+                type:String
+            },
+            selector:{
+                type: String
+            },
+            src: {
+                type: String,
+            },
+        };
+    }
 
-  connectedCallback() {
-    if (this.hasAttribute('label')) {
-      this.label = this.getAttribute('label');
-    }
-    if (this.hasAttribute('name')) {
-      this.name = this.getAttribute('name');
-    }
-    if (this.hasAttribute('selected')) {
-      this.selected = this.getAttribute('selected');
-    }
+    /*
+      constructor() {
+        super();
+      }
+    */
 
-    const style = `
+    connectedCallback() {
+        if (this.hasAttribute('label')) {
+            this.label = this.getAttribute('label');
+        }
+        if (this.hasAttribute('name')) {
+            this.name = this.getAttribute('name');
+        }
+        if (this.hasAttribute('selected')) {
+            this.selected = this.getAttribute('selected');
+        }
+        if (this.hasAttribute('selector')) {
+            this.selector = this.hasAttribute('selector') ? this.getAttribute('selector'): 'fx-fore';
+        }
+        if (this.hasAttribute('src')) {
+            this.src = this.getAttribute('src');
+        }
+
+        const style = `
             :host {
                 visibility: none;
             }
         `;
-    const html = `
+        const html = `
            ${this.label ? `<span>${this.label}</span>` : ''}
            <slot></slot>
         `;
-    this.shadowRoot.innerHTML = `
+        this.shadowRoot.innerHTML = `
             <style>
                 ${style}
             </style>
             ${html}
-    `;
+        `;
 
-  }
+        this.addEventListener('select', () =>{
+            if(this.src){
+                this._loadFromSrc()
+            }
+            this.getOwnerForm().refresh(true);
+        });
+    }
+
+    /**
+     * loads a Fore from an URL given by `src`.
+     *
+     * Will extract the `fx-fore` element from that target file and use and replace current `fx-fore` element with the loaded one.
+     * @private
+     */
+    async _loadFromSrc() {
+        // console.log('########## loading Fore from ', this.src, '##########');
+        await Fore.loadForeFromSrc(this, this.src, this.selector);
+    }
+
 }
 
 if (!customElements.get('fx-case')) {
-  window.customElements.define('fx-case', FxCase);
+    window.customElements.define('fx-case', FxCase);
 }
