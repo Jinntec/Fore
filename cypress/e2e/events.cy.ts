da9f0497121ece43a07ea40b471b00222415f66d
Joern Turner <joern@jinntec.de>
2025-02-07 17:32:01 +0100

    cypress test replacing unit test

---
+++ cypress/e2e/events.cy.ts
@@ -0,0 +1,10 @@
+describe.only('binding.html', () => {
+	beforeEach(() => {
+		cy.visit('test/events.html');
+	});
+
+	it('handles bubbling event"', () => {
+		cy.get('#result').should('contain.text', 'group');
+	});
+
+});
