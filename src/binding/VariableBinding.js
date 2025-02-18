5d18b0ad2df4ab6c0eb3555e06cffc872e36c505
Joern Turner <joern@jinntec.de>
2025-02-18 11:59:02 +0100

    new VariableBinding class

---
+++ src/binding/VariableBinding.js
@@ -0,0 +1,12 @@
+import {Binding} from "./Binding";
+
+export class VariableBinding extends Binding{
+
+    constructor(xpath) {
+        super(xpath,'variable');
+    }
+
+    update() {
+        super.update();
+    }
+}
