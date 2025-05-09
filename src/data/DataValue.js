38d4462ea3a9c9eb360d03c809ba175e3f4aef1b
Joern Turner <joern@jinntec.de>
2025-05-09 12:15:01 +0200

    net yet used

--- src/data/DataValue.js
+++ src/data/DataValue.js
@@ -1,4 +1,4 @@
-export class DataValue{
+export class DataValue {
     constructor(value) {
         this.dataValue = value;
     }
@@ -6,11 +6,10 @@ export class DataValue{
     get dataValue(){
         // to be implemented by overwriting class
         return this.dataValue;
-    };
+    }
 
     set dataValue(newVal){
         // to be implemented by overwriting class
         this.dataValue = newVal;
     }
-
 }
