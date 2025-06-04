390466f2619f6f675d3c583b7da88f22a304a47d
Joern Turner <joern@jinntec.de>
2025-06-04 16:01:56 +0200

    move activate to UIElement

--- src/ui/UIElement.js
+++ src/ui/UIElement.js
@@ -1,5 +1,5 @@
-import ForeElementMixin from '../ForeElementMixin';
-import { Fore } from '../fore';
+import ForeElementMixin from '../ForeElementMixin.js';
+import { Fore } from '../fore.js';
 
 export class UIElement extends ForeElementMixin {
   constructor() {
@@ -16,6 +16,16 @@ export class UIElement extends ForeElementMixin {
       });
       this.addTrashIcon();
     }
+  }
+
+  activate() {
+    console.log('UIElement.activate() called');
+    this.removeAttribute('on-demand');
+    this.style.display = '';
+    if (this.isBound()) {
+      this.refresh(true);
+    }
+    Fore.dispatch(this, 'show-group', {});
   }
 
   attributeChangedCallback(name, _oldValue, newValue) {
@@ -44,10 +54,18 @@ export class UIElement extends ForeElementMixin {
     const trash = this.querySelector('.trash');
     if (trash) return;
     const icon = document.createElement('span');
-    icon.innerHTML = '&#128465;'; // trash icon
+    // icon.innerHTML = '&#128465;'; // trash icon
+    icon.innerHTML = `
+  <svg width="24" height="24" viewBox="0 0 24 24" fill="none"
+       stroke="currentColor" stroke-width="2" stroke-linecap="round"
+       stroke-linejoin="round" xmlns="http://www.w3.org/2000/svg">
+    <path d="M17.94 17.94C16.13 19.12 14.13 20 12 20C7 20 2.73 15.88 1 12C1.6 10.66 2.43 9.47 3.46 8.48M10.58 10.58C10.21 11.01 10 11.5 10 12C10 13.11 10.89 14 12 14C12.5 14 12.99 13.79 13.42 13.42M6.53 6.53C7.87 5.54 9.39 5 12 5C17 5 21.27 9.12 23 12C22.4 13.34 21.57 14.53 20.54 15.52M1 1L23 23"/>
+  </svg>
+`;
+
     icon.classList.add('trash');
-    icon.setAttribute('title', 'Hide this control');
-    icon.setAttribute('part', 'trash');
+    icon.setAttribute('title', 'Hide');
+    // icon.setAttribute('part', 'trash');
     icon.style.cursor = 'pointer';
     icon.style.marginLeft = '0.5em';
 
