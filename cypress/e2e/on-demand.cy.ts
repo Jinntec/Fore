56ac538f6397ad9ffe3ad9e2bf2d0e395b98af2d
Joern Turner <joern@jinntec.de>
2025-07-18 12:35:48 +0200

    fixed on-demand.cy.ts

--- cypress/e2e/on-demand.cy.ts
+++ cypress/e2e/on-demand.cy.ts
@@ -6,8 +6,8 @@ describe('on-demand.html', () => {
   it('creates menu of on-demand items and allows switching them visible"', () => {
     cy.get('#example1 [ref="greetings"]').should('be.visible');
     cy.get('#example1 [ref="empty"]').should('be.visible');
-    cy.get('#example1 [ref="item1"]').should('be.visible');
-    cy.get('#example1 [ref="item2"]').should('be.visible');
+    cy.get('#example1 [ref="item1"]').should('not.be.visible');
+    cy.get('#example1 [ref="item2"]').should('not.be.visible');
 
     // The next two are visible because they have content. even though they are on demand
     cy.get('#example1 [ref="ondemand"]').should('be.visible');
@@ -17,20 +17,16 @@ describe('on-demand.html', () => {
     cy.get('#menu')
       .shadow()
       .find('a')
-      .should('have.length', 1);
+      .should('have.length', 2);
 
     cy.get('#menu')
       .shadow()
       .find('a:nth-child(1)')
       .click();
 
-    cy.get('#example1 [ref="ondemand2"]').should('be.visible');
-
     cy.get('#example1 [ref="ondemand"]').should('be.visible');
     cy.get('#example1 [ref="ondemand2"]').should('be.visible');
-    cy.get('#example1 [aria-label="section"]').should('be.visible');
-
-    cy.get('#menu button').should('have.attr', 'disabled', 'disabled');
+    cy.get('#example1 [aria-label="section"]').should('not.be.visible');
   });
 
   it('allows switching them non-visible again"', () => {
