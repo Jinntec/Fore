4d74b782c43c24460129ac88a90af778f765e14f
Martin Middel <martin@elliat.nl>
2025-11-26 09:03:15 +0100

    fix(create-nodes): default to appending nodes instead of prepending if no bind is found
    More predictable. Also add a test

--- test/create-nodes.test.js
+++ test/create-nodes.test.js
@@ -108,7 +108,90 @@ describe('create-nodes', () => {
     const inst = document.querySelector('fx-instance');
 
     expect(inst.instanceData.documentElement.innerHTML.replaceAll(/\s/g, '')).to.equal(
-      `<root><foo>FOO</foo><baz/><bar>BAR</bar><new-elementrole="special"/><newest-elementrole="special"><extra-specialspecialness="extreme"/></newest-element><a><very><deeppath="here"><and><nowi="map"><toanAttribute=""/></now></and></deep></very></a></root>`,
+      `<root>
+        <foo>FOO</foo>
+        <baz />
+        <bar>BAR</bar>
+        <new-elementrole ="special" />
+        <newest-element role="special">
+          <extra-special specialness="extreme" />
+        </newest-element>
+        <a>
+          <very>
+            <deep path="here">
+              <and>
+                <now i="map"><to anAttribute="" /> </now>
+              </and>
+            </deep>
+          </very>
+        </a>
+      </root>`.replace(/\s/g, ''),
+    );
+  });
+
+  it('uses binds to choose between positions', async () => {
+    const el = fixtureSync(html`
+      <fx-fore create-nodes>
+        <fx-model>
+          <fx-instance>
+            <data>
+              <root>
+                <foo>FOO</foo>
+                <bar>BAR</bar>
+              </root>
+            </data>
+          </fx-instance>
+
+          <fx-bind ref="root">
+            <fx-bind ref="foo"></fx-bind>
+            <fx-bind ref="baz"></fx-bind>
+            <fx-bind ref="between-baz-and-bar"></fx-bind>
+            <fx-bind ref="bar"></fx-bind>
+            <fx-bind ref="new-element"></fx-bind>
+          </fx-bind>
+        </fx-model>
+
+        <fx-group ref="root">
+          <fx-control ref="foo"></fx-control>
+          <fx-control ref="baz"></fx-control>
+          <fx-control ref="bar"></fx-control>
+          <fx-control ref="new-element[@role='special']"></fx-control>
+          <fx-control ref="newest-element[@role='special']/extra-special[@specialness='extreme']">
+          </fx-control>
+          <fx-group ref="a/very/deep[@path='here']">
+            <fx-control ref="and/now[@i='map']/to/@anAttribute"></fx-control>
+          </fx-group>
+          <fx-control ref="between-baz-and-bar"></fx-control>
+        </fx-group>
+      </fx-fore>
+    `);
+
+    await oneEvent(el, 'ready');
+
+    const inst = document.querySelector('fx-instance');
+
+    expect(inst.instanceData.documentElement.innerHTML.replaceAll(/\s/g, '')).to.equal(
+      `<root>
+        <foo>FOO</foo>
+        <baz />
+        <between-baz-and-bar />
+        <bar>BAR</bar>
+        <new-element role="special" />
+        <newest-element role="special">
+          <extra-special specialness="extreme" />
+        </newest-element>
+        <a>
+          <very>
+            <deep path="here">
+              <and>
+                <now i="map">
+                  <to anAttribute="" />
+                </now>
+              </and>
+            </deep>
+          </very>
+        </a>
+      </root>`.replaceAll(/\s/g, ''),
     );
   });
 });
