2be6c90a81c7cb21ac44608e2ae867720900be95
Joern Turner <joern@jinntec.de>
2025-12-08 14:14:24 +0100

    updated demo-build.js

--- demo/demo-build.js
+++ demo/demo-build.js
@@ -2,7 +2,6 @@
 import '@polymer/paper-input/paper-input.js';
 import '@polymer/paper-checkbox/paper-checkbox.js';
 import '@polymer/paper-button/paper-button.js';
-import './js/demo-snippet.js';
-import '@polymer/iron-demo-helpers/demo-pages-shared-styles.js';
-import './codemirror/jinn-codemirror-bundle.js';
+import './js/demo-snippet-vanilla.js';
 import '../index.js';
+import '../doc/fore-corner.js';
