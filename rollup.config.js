bcab1d774480e73013421c867e87a65976d72586
Joern Turner <joern@jinntec.de>
2026-03-13 17:24:15 +0100

    rollback

--- rollup.config.js
+++ rollup.config.js
@@ -1,21 +1,19 @@
 import resolve from '@rollup/plugin-node-resolve';
-import babel from '@rollup/plugin-babel';
+import babel from 'rollup-plugin-babel';
 import { terser } from 'rollup-plugin-terser';
 import minifyHTML from 'rollup-plugin-minify-html-literals';
 import strip from '@rollup/plugin-strip';
 import versionInjector from 'rollup-plugin-version-injector';
-
-const extensions = ['.js'];
 
 // eslint-disable-next-line no-unused-vars
 const { dependencies } = require('./package.json');
 
 export default [
   {
-    input: './demo/demo-build.js',
+    input: './index-build.js',
     output: [
       {
-        file: 'demo/demo.js',
+        file: 'dist/fore.js',
         format: 'es',
         sourcemap: false,
       },
@@ -25,16 +23,38 @@ export default [
       versionInjector(),
       resolve(),
       babel({
-        babelHelpers: 'bundled',
         babelrc: false,
-        extensions,
         plugins: [
+          // Tell babel to accept the `static READONLY_DEFAULT = false;` properties found in some places.
+          // TODO: reconsider whether that is a good idea.
+          // eslint-disable-next-line global-require
           [require('@babel/plugin-proposal-class-properties'), { loose: true }],
-          [require('@babel/plugin-proposal-private-methods'), { loose: true }],
         ],
       }),
       strip(),
       minifyHTML(),
     ],
-  }
+  },
+  {
+    input: './index.js',
+    output: [
+      {
+        file: 'dist/fore-dev.js',
+        format: 'es',
+        sourcemap: false,
+      },
+    ],
+    plugins: [
+      versionInjector(),
+      resolve(),
+      babel({
+        babelrc: false,
+        plugins: [
+          // Tell babel to accept the `static READONLY_DEFAULT = false;` properties found in some places.
+          // eslint-disable-next-line global-require
+          [require('@babel/plugin-proposal-class-properties'), { loose: true }],
+        ],
+      }),
+    ],
+  },
 ];
