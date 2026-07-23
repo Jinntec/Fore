915186dfd4499957b10a146b8c9f5f0cda2f3da8
Martin Middel <martin@elliat.nl>
2026-07-23 11:05:54 +0200

    fix(create-nodes): allow settng the values of elements in createnodes
    And refactor a bit, process more as path steps, and see both attributes and elements as value tests
    
    Fixes #362

--- test/createNodes.test.js
+++ test/createNodes.test.js
@@ -94,6 +94,15 @@ describe('createNodes', () => {
     expect(result).to.equal(null, 'The result should be null');
   });
 
+  it('Does not set the values of elements outside of predicates', () => {
+    const result = createNodes(
+      `InvoiceLine/Item/AdditionalItemProperty/Name='RightType'`,
+      baseElement,
+      foreElement,
+    );
+    expect(result).to.equal(null, 'The result should be null');
+  });
+
   describe('recursive processing', () => {
     it('can make a path with new expressions in the predicate', () => {
       const xpath = 'a[b/c]';
@@ -117,6 +126,19 @@ describe('createNodes', () => {
       expect(result).to.not.equal(null, 'The result should not be null');
 
       expect(result.outerHTML).to.equal('<name><name attr="value"/></name>');
+    });
+
+    it('Can set values of elements in predicates', () => {
+      const result = createNodes(
+        `InvoiceLine/Item/AdditionalItemProperty[Name='RightType']/Value`,
+        baseElement,
+        foreElement,
+      );
+      expect(result).to.not.equal(null, 'The result should not be null');
+
+      expect(result.outerHTML).to.equal(
+        '<InvoiceLine><Item><AdditionalItemProperty><Name>RightType</Name><Value/></AdditionalItemProperty></Item></InvoiceLine>',
+      );
     });
   });
 });
